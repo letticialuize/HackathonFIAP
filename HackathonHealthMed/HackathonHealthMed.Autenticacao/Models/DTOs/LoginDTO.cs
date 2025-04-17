ee8dd204c1e01b76273a3a78467443db73f54627
letticialuize <letticialuize@gmail.com>
2025-04-16 22:50:26 -0300

    feat: Finalizando etapa de login por cpf/email ou CRM

--- HackathonHealthMed/HackathonHealthMed.Autenticacao/Models/DTOs/LoginDTO.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace HackathonHealthMed.Autenticacao.Models.DTOs
-{
-    public class LoginDTO
-    {
-        public string Email { get; set; }
-        public string Senha { get; set; }
-    }
-}
---
+++ HackathonHealthMed/HackathonHealthMed.Autenticacao/Models/DTOs/LoginMedicoDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HackathonHealthMed.Autenticacao.Models.DTOs
+{
+    public class LoginMedicoDTO
+    {
+        [Required(ErrorMessage = "O CRM é obrigatório.")]
+        public string CRM { get; set; }
+        
+        [Required(ErrorMessage = "A senha é obrigatória.")]
+        public string Senha { get; set; }
+    }
+}
