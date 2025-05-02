567d43fc5281615fab5abf69ca8f2c542998b49a
Leonardo <leo.t.n@hotmail.com>
2025-05-02 12:18:38 -0300

    Configurações inicias do projeto de gestão de consultas, como configuração de swagger e EF

--- HackathonHealthMed/HackathonHealthMed.GestaoConsultas/Program.cs
+++ HackathonHealthMed/HackathonHealthMed.GestaoConsultas/Program.cs
@@ -1,8 +1,39 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Agendamento", Version = "v1" });
+
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "JWT Authorization header using the Bearer scheme. " +
+        "Enter 'Bearer'[space] and then your token in the text input below.",
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                          new OpenApiSecurityScheme
+                          {
+                              Reference = new OpenApiReference
+                              {
+                                  Type = ReferenceType.SecurityScheme,
+                                  Id = "Bearer"
+                              }
+                          },
+                         new string[] {}
+                    }
+                });
+});
 
 var app = builder.Build();
 
